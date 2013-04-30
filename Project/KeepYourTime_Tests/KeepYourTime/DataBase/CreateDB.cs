42a01a9fd126843aa244cac146fdc5e7bf4fa0be
ganhotoman@gmail.com <ganhotoman@gmail.com@f765a11b-06fa-48ab-b221-572212c6e30c>
2013-04-30 00:19:49 +0000

    Runtime DB Creation

---
+++ Project/KeepYourTime_Tests/KeepYourTime/DataBase/CreateDB.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlServerCe;
+using System.Data;
+
+namespace KeepYourTime.DataBase
+{
+    class CreateDB
+    {
+        public static bool IsDbCreated()
+        {
+            return (File.Exists(DBUtils.FileName));
+        }
+
+        public void CreateDatabase()
+        {
+            string connectionString = DBUtils.GetConnectionString();
+            SqlCeEngine en = new SqlCeEngine(connectionString);
+            en.CreateDatabase();
+
+            CreateTableTarefa();
+            CreateTableTempo();
+
+        }
+
+        private void CreateTableTask()
+        {
+            string strSQL = "CREATE TABLE Task( " + 
+                "ID BIGINT IDENTITY NOT NULL, " +
+                "" +
+                "" +
+                "" +
+                "" +
+                "CONSTRAINT PK_Task PRIMARY KEY (ID) " +
+                ")";
+
+            DBUtils.ExecuteOperation(strSQL);
+        }
+
+        private void CreateTableTime()
+        {
+            string strSQL = "CREATE TABLE Time( " +
+                "ID BIGINT IDENTITY NOT NULL, " +
+                "IDTarefa BIGINT NOT NULL, " +
+                "" +
+                "" +
+                "" +
+                "" +
+                "CONSTRAINT PK_Time PRIMARY KEY (ID) " +
+                ")";
+
+            DBUtils.ExecuteOperation(strSQL);
+        }
+
+        private void CreateTableDefinitions()
+        { }
+
+        private void CreateForeignKeys()
+        {
+            var strSQL = "";
+
+        }
+
+        private void InsertValues()
+        { }
+
+    }
+}
