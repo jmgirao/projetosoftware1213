da5c3be8dd1fa60138db24462ef08cb6aa9bb12f
joaomgirao@gmail.com <joaomgirao@gmail.com@f765a11b-06fa-48ab-b221-572212c6e30c>
2013-05-17 20:11:55 +0000

    user control TaskDetailsControl

--- trunk/Project/KeepYourTime/KeepYourTime/ViewWindows/TaskDetailsWindow.xaml.cs
+++ trunk/Project/KeepYourTime/KeepYourTime/ViewWindows/TaskDetailsWindow.xaml.cs
@@ -26,52 +26,10 @@ namespace KeepYourTime.ViewWindows
     {
         public TaskDetailsWindow()
         {
-            SourceInitialized += Window_Initialized;
             InitializeComponent();
 
-            btCloseTaskDetails.Click += btCloseTaskDetails_Click; 
         }
 
-        /// <summary>
-        /// Handles the Initialized event of the Window control.
-        /// </summary>
-        /// <param name="sender">The source of the event.</param>
-        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
-        private void Window_Initialized(object sender, EventArgs e)
-        {
-            var mhResult = new MethodHandler();
-            int idTask = 0;                 //TODO: To receive idTask another window
-            TaskAdapter taskAdapt = new TaskAdapter();
-
-            try
-            {
-                mhResult = TaskConnector.ReadTask(idTask, out taskAdapt);
-                if (mhResult.Exits)
-                {
-                    MessageBox.Show(mhResult.Message);
-                    return;
-                }
-
-                string taskName = taskAdapt.TaskName;
-                string taskDescription = taskAdapt.Description;
-                ObservableCollection<TaskTimeAdapter> taskTimeList = taskAdapt.Times;
-
-                lbTaskNameText.Text = taskName;
-                lbTaskDescriptionText.Text = taskDescription;
-                dgTaskTimes.DataContext = taskTimeList;
-
-            }catch(Exception ex)
-            {
-                mhResult.Exception(ex);
-                MessageBox.Show(mhResult.Message);
-            }
-
-        }
-
-        private void btCloseTaskDetails_Click(object sender, RoutedEventArgs e)
-        {
-            this.Close();
-        }
 
     }
 }
