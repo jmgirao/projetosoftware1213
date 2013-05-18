404b4d6b2fa1b6c9ad1595cf315d0f9e19b7f4d3
joaomgirao@gmail.com <joaomgirao@gmail.com@f765a11b-06fa-48ab-b221-572212c6e30c>
2013-05-18 12:03:36 +0000

    add TaskTimeAdapterUI Add timeSpent in datagrid

---
+++ Project/KeepYourTime/KeepYourTime/ViewControls/TaskDetailsControls/TaskTimeAdapterUI.cs
@@ -0,0 +1,34 @@
+﻿using KeepYourTime.DataBase.Adapters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace KeepYourTime.ViewControls.TaskDetailsControls
+{
+    /// <summary>
+    /// Class TaskTimeAdapterUI
+    /// </summary>
+    /// <remarks>CREATED BY João Girão</remarks>
+    class TaskTimeAdapterUI : TaskTimeAdapter
+    {
+
+        public string TimeSpent
+        {
+            get
+            {
+                TimeSpan tsTimeSpent = this.StopTime.Subtract(this.StartTime);
+                return tsTimeSpent.Hours + ":" + tsTimeSpent.Minutes + ":" + tsTimeSpent.Seconds;
+            }
+            set { }
+        }
+
+        public TaskTimeAdapterUI(TaskTimeAdapter baseTaskTimer)
+        {
+            this.TimeId = baseTaskTimer.TimeId;
+            this.TaskId = baseTaskTimer.TaskId;
+            this.StartTime = baseTaskTimer.StartTime;
+            this.StopTime = baseTaskTimer.StopTime;
+        }
+    }
+}
