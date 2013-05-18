4d2dc0776aa1207370bba1248ed8291d370f7b22
mariojboliveira@gmail.com <mariojboliveira@gmail.com@f765a11b-06fa-48ab-b221-572212c6e30c>
2013-05-18 10:12:01 +0000

    tratei finally

--- trunk/Project/KeepYourTime/KeepYourTime/ViewControls/MainWindowControls/TaskAdapterUI.cs
+++ trunk/Project/KeepYourTime/KeepYourTime/ViewControls/MainWindowControls/TaskAdapterUI.cs
@@ -9,13 +9,22 @@ namespace KeepYourTime.ViewControls.MainWindowControls
 {
     class TaskAdapterUI : TaskAdapter
     {
+        /*public string TotalTime { get { return "XXX:XX"; } }
+        public string TodayTime { get { return "XX:XX"; } }
+        public string StopDate { get { return "12/41/1231"; } }*/
+
         public TaskAdapterUI()
         {
+            
+          
         }
 
         public TaskAdapterUI(TaskAdapter taskBase)
-        { 
+        {
+            //base(taskBase.TotalTime, taskBase.TodayTime, taskBase, StopDate);
 
         }
+        
+        
     }
 }
