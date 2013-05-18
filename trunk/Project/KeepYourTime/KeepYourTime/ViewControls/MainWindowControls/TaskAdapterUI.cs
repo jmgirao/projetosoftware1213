6841ad555b1d21128c68b7b2e52375afee52188a
ganhotoman@gmail.com <ganhotoman@gmail.com@f765a11b-06fa-48ab-b221-572212c6e30c>
2013-05-18 08:42:42 +0000

    TaskAdapterUI

---
+++ trunk/Project/KeepYourTime/KeepYourTime/ViewControls/MainWindowControls/TaskAdapterUI.cs
@@ -0,0 +1,20 @@
+﻿using KeepYourTime.DataBase.Adapters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KeepYourTime.ViewControls.MainWindowControls
+{
+    class TaskAdapterUI : TaskAdapter
+    {
+        public TaskAdapterUI()
+        {
+        }
+
+        public TaskAdapterUI(TaskAdapter taskBase)
+        { 
+        }
+    }
+}
