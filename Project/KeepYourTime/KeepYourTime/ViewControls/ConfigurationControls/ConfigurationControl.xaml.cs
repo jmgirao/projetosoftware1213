acdb183e14647b58345723bb35bbccd993514e12
longinus.boy@gmail.com <longinus.boy@gmail.com@f765a11b-06fa-48ab-b221-572212c6e30c>
2013-05-24 14:44:16 +0000

    Configurating shortcuts almost done (combo boxes actions needs more work)

--- Project/KeepYourTime/KeepYourTime/ViewControls/ConfigurationControls/ConfigurationControl.xaml.cs
+++ Project/KeepYourTime/KeepYourTime/ViewControls/ConfigurationControls/ConfigurationControl.xaml.cs
@@ -1,6 +1,9 @@
-﻿using KeepYourTime.Utils;
+﻿using KeepYourTime.DataBase.Adapters;
+using KeepYourTime.DataBase.Connectors;
+using KeepYourTime.Utils;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -25,6 +28,63 @@ namespace KeepYourTime.ViewControls.ConfigurationControls
         {
             InitializeComponent();
             btCancel.Click += btCancel_Click;
+            btApply.Click += btApply_Click;
+            var mhResult = new MethodHandler();
+            try
+            {
+                ObservableCollection<TaskAdapter> taskList;
+
+                mhResult = TaskConnector.ReadTaskList(out taskList, false);
+
+                cbShort1.Items.Add("");
+                cbShort2.Items.Add("");
+                cbShort3.Items.Add("");
+                cbShort4.Items.Add("");
+                cbShort5.Items.Add("");
+
+                for (int i = 0; i < taskList.Count; i++)
+                {
+                    cbShort1.Items.Add(taskList[i].TaskName);
+                    cbShort2.Items.Add(taskList[i].TaskName);
+                    cbShort3.Items.Add(taskList[i].TaskName);
+                    cbShort4.Items.Add(taskList[i].TaskName);
+                    cbShort5.Items.Add(taskList[i].TaskName);
+                }
+
+                ConfigurationAdapter configuration;
+                mhResult = ConfigurationConnector.ReadConfiguration(out configuration);
+                List<ShortcutAdapter> listShortcuts = configuration.Shortcuts;
+
+                //TODO must select the task name for all the combo boxes
+
+                chkShift1.IsChecked = listShortcuts[0].Shift;
+                chkShift2.IsChecked = listShortcuts[1].Shift;
+                chkShift3.IsChecked = listShortcuts[2].Shift;
+                chkShift4.IsChecked = listShortcuts[3].Shift;
+                chkShift5.IsChecked = listShortcuts[4].Shift;
+
+                chkCtrl1.IsChecked = listShortcuts[0].Ctrl;
+                chkCtrl2.IsChecked = listShortcuts[1].Ctrl;
+                chkCtrl3.IsChecked = listShortcuts[2].Ctrl;
+                chkCtrl4.IsChecked = listShortcuts[3].Ctrl;
+                chkCtrl5.IsChecked = listShortcuts[4].Ctrl;
+
+                chkAlt1.IsChecked = listShortcuts[0].Alt;
+                chkAlt2.IsChecked = listShortcuts[1].Alt;
+                chkAlt3.IsChecked = listShortcuts[2].Alt;
+                chkAlt4.IsChecked = listShortcuts[3].Alt;
+                chkAlt5.IsChecked = listShortcuts[4].Alt;
+
+                txtShortcutKey1.Text = listShortcuts[0].ShortcutKey.ToString();
+                txtShortcutKey2.Text = listShortcuts[1].ShortcutKey.ToString();
+                txtShortcutKey3.Text = listShortcuts[2].ShortcutKey.ToString();
+                txtShortcutKey4.Text = listShortcuts[3].ShortcutKey.ToString();
+                txtShortcutKey5.Text = listShortcuts[4].ShortcutKey.ToString();
+            }
+            catch (Exception e)
+            {
+                mhResult.Exception(e);
+            }
         }
 
         void btCancel_Click(object sender, RoutedEventArgs e)
@@ -43,6 +103,95 @@ namespace KeepYourTime.ViewControls.ConfigurationControls
             {
                 CsvExporter.ExportDatabaseToCSV(dialog.FileName);
             
+            }
+        }
+
+        private bool isValideShortcuts()
+        {
+            if (cbShort1.SelectedIndex != 0 && txtShortcutKey1.Text.Trim() == "")
+                return false;
+            if (cbShort2.SelectedIndex != 0 && txtShortcutKey2.Text.Trim() == "")
+                return false;
+            if (cbShort3.SelectedIndex != 0 && txtShortcutKey3.Text.Trim() == "")
+                return false;
+            if (cbShort4.SelectedIndex != 0 && txtShortcutKey4.Text.Trim() == "")
+                return false;
+            if (cbShort5.SelectedIndex != 0 && txtShortcutKey5.Text.Trim() == "")
+                return false;
+            return true;
+        }
+
+        private void btApply_Click(object sender, RoutedEventArgs e)
+        {
+            if (!isValideShortcuts())
+            {
+                MessageBox.Show("One shortcut appear to be incomplete, you need to choose any character for it",
+                    "Incomplete shortcut");
+                return;
+            }
+
+
+            var mhResult = new MethodHandler();
+
+            //TODO taskid must be correctly "generated/readed" and be saved to list
+            try
+            {
+                List<ShortcutAdapter> listShortcuts = new List<ShortcutAdapter>();
+                listShortcuts.Add(new ShortcutAdapter
+                {
+                    TaskId = 1,
+                    Shift = (chkShift1.IsChecked.HasValue) ? chkShift1.IsChecked.Value : false,
+                    Ctrl = (chkCtrl1.IsChecked.HasValue) ? chkCtrl1.IsChecked.Value : false,
+                    Alt = (chkAlt1.IsChecked.HasValue) ? chkAlt1.IsChecked.Value : false,
+                    ShortcutKey = txtShortcutKey1.Text[0]
+                });
+                listShortcuts.Add(new ShortcutAdapter
+                {
+                    TaskId = 1,
+                    Shift = (chkShift2.IsChecked.HasValue) ? chkShift2.IsChecked.Value : false,
+                    Ctrl = (chkCtrl2.IsChecked.HasValue) ? chkCtrl2.IsChecked.Value : false,
+                    Alt = (chkAlt2.IsChecked.HasValue) ? chkAlt2.IsChecked.Value : false,
+                    ShortcutKey = txtShortcutKey2.Text[0]
+                });
+
+                listShortcuts.Add(new ShortcutAdapter
+                {
+                    TaskId = 1,
+                    Shift = (chkShift3.IsChecked.HasValue) ? chkShift3.IsChecked.Value : false,
+                    Ctrl = (chkCtrl3.IsChecked.HasValue) ? chkCtrl3.IsChecked.Value : false,
+                    Alt = (chkAlt3.IsChecked.HasValue) ? chkAlt3.IsChecked.Value : false,
+                    ShortcutKey = txtShortcutKey3.Text[0]
+                });
+
+                listShortcuts.Add(new ShortcutAdapter
+                {
+                    TaskId = 1,
+                    Shift = (chkShift4.IsChecked.HasValue) ? chkShift4.IsChecked.Value : false,
+                    Ctrl = (chkCtrl4.IsChecked.HasValue) ? chkCtrl4.IsChecked.Value : false,
+                    Alt = (chkAlt4.IsChecked.HasValue) ? chkAlt4.IsChecked.Value : false,
+                    ShortcutKey = txtShortcutKey4.Text[0]
+                });
+
+                listShortcuts.Add(new ShortcutAdapter
+                {
+                    TaskId = 1,
+                    Shift = (chkShift5.IsChecked.HasValue) ? chkShift5.IsChecked.Value : false,
+                    Ctrl = (chkCtrl5.IsChecked.HasValue) ? chkCtrl5.IsChecked.Value : false,
+                    Alt = (chkAlt5.IsChecked.HasValue) ? chkAlt5.IsChecked.Value : false,
+                    ShortcutKey = txtShortcutKey5.Text[0]
+                });
+
+                var cf = new ConfigurationAdapter();
+                cf.Inactivity = (chkInactivityAlert.IsChecked.HasValue) ? chkInactivityAlert.IsChecked.Value : false;
+                //cf.InactivityTime = 5;
+                cf.Shortcuts = listShortcuts;
+                mhResult = ConfigurationConnector.SaveConfiguration(cf);
+
+                Window.GetWindow(this).Close();
+            }
+            catch (Exception ex1)
+            {
+                mhResult.Exception(ex1);
             }
         }
     }
