d7d95282505f3756a035972075b4f18ade9b4581
ganhotoman@gmail.com <ganhotoman@gmail.com@f765a11b-06fa-48ab-b221-572212c6e30c>
2013-05-29 22:52:58 +0000

    KeyBoard Algorythm Done

--- trunk/Project/KeepYourTime/KeepYourTime/Utils/CurrentConfigurations.cs
+++ trunk/Project/KeepYourTime/KeepYourTime/Utils/CurrentConfigurations.cs
@@ -1,10 +1,12 @@
 ﻿using KeepYourTime.DataBase.Adapters;
 using KeepYourTime.DataBase.Connectors;
+using KeepYourTime.Hooks;
 using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace KeepYourTime.Utils
 {
@@ -53,6 +55,57 @@ namespace KeepYourTime.Utils
 
 
 
+        }
+
+
+        private static List<Hooks.Hotkey> lstHotKeys = new List<Hooks.Hotkey>();
+
+        public static KeepYourTime.ViewWindows.MainWindow mw;
+
+        public static void ConfigureHotKeys()
+        {
+
+            while (lstHotKeys.Count > 0)
+            {
+                lstHotKeys.First().Unregister();
+                lstHotKeys.RemoveAt(0);
+            }
+
+
+            foreach (ShortcutAdapter sc in allConfig.Shortcuts)
+            {
+                if ((sc.ShortcutKey != ""))
+                {
+                    var hkHotKey = new Hotkey(GetKeyByString(sc.ShortcutKey), sc.Shift, sc.Ctrl, sc.Alt, false);
+                    hkHotKey.TaskID = sc.TaskId;
+
+                    hkHotKey.Pressed += hkHotKey_Pressed;
+
+
+
+                    hkHotKey.Register(mw);
+                }
+
+            }
+        }
+
+        static void hkHotKey_Pressed(object sender, System.ComponentModel.HandledEventArgs e)
+        {
+            //TODO: Start Task
+            long TaskID = (sender as Hotkey).TaskID;
+
+            //TODO: IF TASK NOT AVAILABLE MAKE SOUND
+
+            e.Handled = true;
+        }
+
+        static private Keys GetKeyByString(string c)
+        {
+
+            if (c.Length == 0)
+                return Keys.None;
+            c = c.ToUpper();
+            return (Keys)c[0];
         }
 
     }
