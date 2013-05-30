324916e3871e0a0224a1c2174bac5878710222db
mariojboliveira@gmail.com <mariojboliveira@gmail.com@f765a11b-06fa-48ab-b221-572212c6e30c>
2013-05-30 15:18:07 +0000

    deny delete a running task deny deactive a running task read TotalTime, TodayTime and StopDate

--- trunk/Project/KeepYourTime/KeepYourTime/ViewControls/MainWindowControls/TaskAdapterUI.cs
+++ trunk/Project/KeepYourTime/KeepYourTime/ViewControls/MainWindowControls/TaskAdapterUI.cs
@@ -3,6 +3,7 @@ using KeepYourTime.DataBase.Connectors;
 using KeepYourTime.ViewWindows;
 using System;
 using System.Collections.Generic;
+using System.Collections.ObjectModel;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -14,9 +15,109 @@ namespace KeepYourTime.ViewControls.MainWindowControls
     /// </summary>
     class TaskAdapterUI : TaskAdapter
     {
-        public string TotalTime { get { return "XXX:XX"; } }
-        public string TodayTime { get { return "XX:XX"; } }
-        public string StopDate { get { return "12/41/1231"; } }
+        public string TotalTime { 
+            get 
+            {
+                String taskAdapTotalTime="";
+                var mhResult = new MethodHandler();
+
+                try
+                {
+
+                    foreach (TaskAdapter t in MainWindow.lstTaskAdapt)
+                    {
+                        if (t.TaskId == this.TaskId)
+                        {
+                            TimeSpan tsTime = TimeSpan.FromSeconds(t.TotalTime);
+                            //taskAdapTotalTime = string.Format("{0:D2}h:{1:D2}m:{2:D2}s",
+                            taskAdapTotalTime = string.Format("{0:D2}:{1:D2}:{2:D2}",
+                                tsTime.Hours,
+                                tsTime.Minutes,
+                                tsTime.Seconds);
+
+                            break;
+                        }
+                    }   
+                }
+                catch (Exception ex)
+                {
+                    mhResult.Exception(ex);
+                    return taskAdapTotalTime;
+                }
+                finally
+                {
+                    MessageWindow.ShowMethodHandler(mhResult, false);
+                }
+                return taskAdapTotalTime;
+            } 
+        }
+        public string TodayTime { 
+            get
+            {
+                String taskAdapTodayTime = "";
+                var mhResult = new MethodHandler();
+
+                try
+                {
+                    foreach (TaskAdapter t in MainWindow.lstTaskAdapt)
+                    {
+                        if (t.TaskId == this.TaskId)
+                        {
+                            TimeSpan tsTime = TimeSpan.FromSeconds(t.TodayTime);
+                            taskAdapTodayTime = string.Format("{0:D2}:{1:D2}:{2:D2}",
+                                tsTime.Hours,
+                                tsTime.Minutes,
+                                tsTime.Seconds);
+                            break;
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    mhResult.Exception(ex);
+                    return taskAdapTodayTime;
+                }
+                finally
+                {
+                    MessageWindow.ShowMethodHandler(mhResult, false);
+                }
+                return taskAdapTodayTime;
+            } 
+        }
+        public string StopDate 
+        { 
+            get 
+            {
+                String taskAdapStopTime = "";
+                var mhResult = new MethodHandler();
+
+                try
+                {
+                    foreach (TaskAdapter t in MainWindow.lstTaskAdapt)
+                    {
+                        if (t.TaskId == this.TaskId)
+                        {
+                            if (t.Times != null)
+                            {
+                                var stTime = t.Times.Max(x => x.StopTime);
+                                taskAdapStopTime = stTime.ToString();
+                                break;
+                            }
+                        }
+                    }
+                }
+                catch (Exception ex)
+                {
+                    mhResult.Exception(ex);
+                    return taskAdapStopTime;
+                }
+                finally
+                {
+                    MessageWindow.ShowMethodHandler(mhResult, false);
+                }
+                return taskAdapStopTime;
+            } 
+        }
 
         public bool ActiveChange
         {
@@ -27,23 +128,29 @@ namespace KeepYourTime.ViewControls.MainWindowControls
             set
             {
                 var mhResult = new MethodHandler();
-                try
-                {
-                    mhResult = TaskConnector.ActivateTask(this.TaskId, !value);
-                    if (mhResult.Exits) return;
-                    this.Active = !value;
 
-                    if (this.Active == false)
-                        if (OnTaskDeactivated != null)
-                            OnTaskDeactivated((object)this, new EventArgs());
-                }
-                catch (Exception ex)
+                if (MinimalViewControl.CurrentTaskId != this.TaskId)
                 {
-                    mhResult.Exception(ex);
-                }
-                finally
-                {
-                    MessageWindow.ShowMethodHandler(mhResult, false);
+
+                    try
+                    {
+
+                        mhResult = TaskConnector.ActivateTask(this.TaskId, !value);
+                        if (mhResult.Exits) return;
+                        this.Active = !value;
+
+                        if (this.Active == false)
+                            if (OnTaskDeactivated != null)
+                                OnTaskDeactivated((object)this, new EventArgs());
+                    }
+                    catch (Exception ex)
+                    {
+                        mhResult.Exception(ex);
+                    }
+                    finally
+                    {
+                        MessageWindow.ShowMethodHandler(mhResult, false);
+                    }
                 }
             }
         }
